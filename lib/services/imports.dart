5428a1595263eb619066687d482593ddcd752c11
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-03-06 16:44:01 +0100

    Cleaned code.

--- lib/services/imports.dart
+++ lib/services/imports.dart
@@ -1,18 +1,21 @@
 export 'dart:io';
-export 'package:flutter/foundation.dart';
+export 'package:flutter/material.dart';
 export 'package:flutter/services.dart';
+export 'package:flutter/foundation.dart';
 
+/// Basic
 export 'package:sqflite_common/sqlite_api.dart';
 export 'package:sqflite_common_ffi/sqflite_ffi.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:url_launcher/url_launcher.dart';
-export 'package:package_info_plus/package_info_plus.dart';
-export 'package:flutter/material.dart';
-export 'package:flutter_easyloading/flutter_easyloading.dart';
 export 'package:url_launcher/url_launcher_string.dart';
+export 'package:package_info_plus/package_info_plus.dart';
 export 'package:auto_updater/auto_updater.dart';
 
-export 'package:lms/constants.dart';
+/// Design
+export 'package:flutter_easyloading/flutter_easyloading.dart';
+
+export '../constants.dart';
 
 export '../interfaces/add_widget.dart';
 export '../interfaces/about_page.dart';
