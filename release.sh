5e23f36bbd39b55b57d0ee0849ae37e836ca2341
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-03-07 17:46:23 +0100

    tested release.sh but not working

--- release.sh
+++ release.sh
@@ -36,10 +36,12 @@ if [ "$release_choice" == "0" ]; then
     echo "Length: $length"
 
     file_path="dist/appcast.xml"
-    replacement="<item>\n<title>Version $release_number</title>\n<sparkle:releaseNotesLink>\nhttps://your_domain/your_path/release_notes.html\n</sparkle:releaseNotesLink>\n<pubDate>Mon, 6 Mar 2023 13:00:00 +0800</pubDate>\n<enclosure url=\"$output\"\nsparkle:edSignature=\"$signature\"\nlength=\"$length\"\nsparkle:version=\"$release_number\"\nsparkle:os=\"macos\"\ntype=\"application/octet-stream\" />\n</item>"
+    replacement="<item>\n<title>Version $release_version</title>\n<sparkle:releaseNotesLink>\nhttps://your_domain/your_path/release_notes.html\n</sparkle:releaseNotesLink>\n<pubDate>Mon, 6 Mar 2023 13:00:00 +0800</pubDate>\n<enclosure url=\"$output\"\nsparkle:edSignature=\"$signature\"\nlength=\"$length\"\nsparkle:version=\"$release_version\"\nsparkle:os=\"macos\"\ntype=\"application/octet-stream\" />\n</item>"
     
+    replacement="<item>\n<title>Version $release_version</title>\n<sparkle:releaseNotesLink>\nhttps://your_domain/your_path/release_notes.html\n</sparkle:releaseNotesLink>\n<pubDate>Mon, 6 Mar 2023 13:00:00 +0800</pubDate>\n<enclosure url=\"$url\"\nsparkle:edSignature=\"$signature\"\nlength=\"$length\"\nsparkle:version=\"$release_version\"\nsparkle:os=\"macos\"\ntype=\"application/octet-stream\" />\n</item>"
+
     # Use sed to replace the text between the #macOS div tags
-    sed -i -e 's|\(<!--macOS_start-->\).*\(<!--macOS_end-->\)|\1'"$replacement"'\2|g' "$file_path"
+    sed -i '' 's|\(<!--macOS_start-->\).*\(<!--macOS_end-->\)|\1'"$replacement"'\2|g' "$file_path"
 
 elif [ "$release_choice" == "1" ]; then
     flutter_distributor release --name dev --jobs release-windows
